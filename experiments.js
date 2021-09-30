2a3adf7f8ef5ff7a8e2f6d3819dab047b4693f00
Sameer Khan <sameerkhan80030@gmail.com>
2021-09-30 10:12:39 +0530

    learn stings in JavaScript

--- experiments.js
+++
@@ -1,5 +0,0 @@
-const monthlyRent = 500;
-console.log(monthlyRent);
-
-const yearlyRent = monthlyRent * 12;
-console.log(yearlyRent);
---
+++ js/strings.js
@@ -0,0 +1,10 @@
+const MyName = 'SAMEER ANSARI';
+console.log(MyName);
+
+
+const FirstName = "Sameer"
+const LastName = "Ansari"
+const sentence = "Hello " + FirstName + " " + LastName + "! How are you !?"
+const sentenceWithTemplate=`Hello ${FirstName} ${LastName}! How are you !?`
+console.log(sentence)
+console.log(sentenceWithTemplate)
