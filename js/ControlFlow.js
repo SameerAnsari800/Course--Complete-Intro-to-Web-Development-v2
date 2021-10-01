6631431e2a7d25c8f5ea37fd0097b22d95e95f5f
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-01 11:27:09 +0530

    learn Control flow in JavaScript

---
+++ js/ControlFlow.js
@@ -0,0 +1,54 @@
+const SkyIsBlue = true;
+if (SkyIsBlue) {
+    console.log("the sky is blue");
+}
+else {
+    console.log("the sky is...not blue?");
+}
+
+
+
+const temperatureToday = 86;
+if (temperatureToday === 86)
+{
+    console.log("it is damn hot");
+}
+else {
+    console.log("it is fine...");
+}
+
+
+
+if (2 + 2 === 4) {
+    console.log("the fundamental principle of mathematics is still hold true")
+}
+else {
+    console.log("UN,PANIC");
+}
+
+
+
+
+
+const temperatureToday12 = '86';  
+if (temperatureToday12 == 86) {
+	console.log('it is damn hot'); // it is  true because == double equal is compare numbers against string
+} else {
+	console.log('it is fine...');
+}
+
+
+
+if (2 + 2 === "4") {
+    console.log("the fundamental principle of mathematics is still hold true") // it is false because === triple equal is compare data types so numbers and string are not equal. it is false  
+}
+else {
+    console.log("UN,PANIC");
+}
+
+
+
+
+
+
+
