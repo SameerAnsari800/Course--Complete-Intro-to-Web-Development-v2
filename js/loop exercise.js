43f8d2842d10b15ac79634f370f28ffbf7d7cb00
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-03 21:26:56 +0530

    learn loop exercise increment,decreament,subtraction,exponent

---
+++ js/loop exercise.js
@@ -0,0 +1,15 @@
+let friendsAtyourParty = 0;
+friendsAtyourParty = friendsAtyourParty + 1;
+friendsAtyourParty +=1;
+friendsAtyourParty++;
+++friendsAtyourParty;
+console.log(friendsAtyourParty);
+
+
+
+let friends = 0;
+for (let i = 0; i <= 10; i++)
+{
+    friends++;
+}
+console.log(friends);
