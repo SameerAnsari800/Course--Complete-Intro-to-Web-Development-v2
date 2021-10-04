ff0e73c39c421d92c374ab6efa2941bd3d6ea4f4
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-04 11:33:23 +0530

    learn Functions and Scope

---
+++ js/Functions and scope.js
@@ -0,0 +1,7 @@
+function greet(FirstName, LastName, honorific, greeting)
+{
+    return `${greeting} ${honorific} ${LastName} ! i'm extremely pleased you could join us,${FirstName} ! i hope you enjoy your stay,${honorific} ${LastName}`;
+
+}
+console.log(greet("SAMEER", "ANSARI", "LORD", "SOLUTATIONS"));
+console.log(greet("BRIAN", "HOLT", "CAPTAIN", "A-HOY"));
