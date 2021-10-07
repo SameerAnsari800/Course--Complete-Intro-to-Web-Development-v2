7e1a99f2eedd7371124befc47929b266c114704c
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-07 13:01:26 +0530

    learn Scope Solutions in javaScript

---
+++ js/Scope Solution.js
@@ -0,0 +1,28 @@
+const A = "A";
+let F;
+function doStuff(B) {
+    console.log(B);
+const C = "c";
+    let H = "h";
+    if (1 + 1 === 2) {
+    const D = "d";
+    H = "Something else";
+}
+console.log(D);
+console.log(H);
+F = "f";
+}
+let E = 0;
+while (E < 3)
+{
+    E++;
+ console.log(A);
+    const G = "g";
+}
+console.log(E);
+console.log(G);
+doStuff("B");
+console.log(B);
+console.log(C);
+console.log(F);
+
