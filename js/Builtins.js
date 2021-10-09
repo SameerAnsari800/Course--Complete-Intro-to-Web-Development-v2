aafe9608cbf08c19ccce2999fd48e8ecc1d7662a
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-09 12:31:44 +0530

    learn Builtins in JavaScript

---
+++ js/Builtins.js
@@ -0,0 +1,11 @@
+const sentence = "this HaS CAsING oN  iT";
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase())
+console.log(Math.round(5.1)); // round down
+console.log(Math.floor(5.9999));// round down
+console.log(Math.ceil(6.1)); //round up 
+
+const Name = "Sameer Ansari";
+console.log(Name.substr(6, 3));
+console.log(Name.substr(4));
+console.log(Name.substring); // the  reason why i dont call that substring is cuz threre's actually one called  
