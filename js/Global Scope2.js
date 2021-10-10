f7b9107cbac44856efe7f91c0308009a6c659370
Sameer Khan <sameerkhan80030@gmail.com>
2021-10-10 12:29:03 +0530

    learn another example of Scope

--- js/Global Scope2.js
+++ js/Global Scope2.js
@@ -1,10 +1,11 @@
 let globalVAr = 'unchange';
 function addFive(number) {
-    globalVAr = 'change';
-    return number + 10;
-
+	globalVAr = 'change';
+	return number + 10;
 }
-console.log(globalVAr);                  /* agar ham globalvar ko addfive ke uper likhenge toh toh dono me unchange hi aaega lekin agar hum ek 
+console.log(
+	globalVAr
+); /* agar ham globalvar ko addfive ke uper likhenge toh toh dono me unchange hi aaega lekin agar hum ek 
                                                         globalvar ko addfive ke uper likhenge or ek globalvar ko addfive ke niche likhenge toh uper wale me toh 'unchange' aaega but niche 
                                                         wale me 'change' aaega kyu ki niche wale globalvar ko addfive function ke ander defined kiya gya hai isiliye  global scope most root scope that exists             like       console.log(globalVar);    console.log(globalVar);     console.log(addFive(20));     */
 
@@ -12,21 +13,8 @@ console.log(addFive(20));
 
 console.log(globalVAr);
 
-
-
-
-
-
-
-
-
-
 let friendsAtyourParty = 0;
-for (let i = 0; i <= 10; i++)
-{
-    friendsAtyourParty++;
+for (let i = 0; i <= 10; i++) {
+	friendsAtyourParty++;
 }
-console.log(i); // error because i is not defined  
-
-
-
+console.log(i); // error because i is not defined
--- js/Scope Solution.js
+++ js/Scope Solution.js
@@ -1,28 +1,26 @@
-const A = "A";
+const A = 'A';
 let F;
 function doStuff(B) {
-    console.log(B);
-const C = "c";
-    let H = "h";
-    if (1 + 1 === 2) {
-    const D = "d";
-    H = "Something else";
-}
-console.log(D);
-console.log(H);
-F = "f";
+	console.log(B);
+	const C = 'c';
+	let H = 'h';
+	if (1 + 1 === 2) {
+		const D = 'd';
+		H = 'Something else';
+	}
+	console.log(D);
+	console.log(H);
+	F = 'f';
 }
 let E = 0;
-while (E < 3)
-{
-    E++;
- console.log(A);
-    const G = "g";
+while (E < 3) {
+	E++;
+	console.log(A);
+	const G = 'g';
 }
 console.log(E);
 console.log(G);
-doStuff("B");
+doStuff('B');
 console.log(B);
 console.log(C);
 console.log(F);
-
